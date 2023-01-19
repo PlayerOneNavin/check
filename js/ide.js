dcd6a397908dd8c0a34eb52c7c937852bcbe3ec4
Navin <goognav01@gmail.com>
2023-01-19 23:36:47 +0530

    network error running button fixed

--- js/ide.js
+++ js/ide.js
@@ -37,109 +37,113 @@ function btnReset(elem) {
 
 async function executeCode(val) {
 
-    btnLoading(val); // disable button and create a Running... button text
+    try {
+        btnLoading(val); // disable button and create a Running... button text
 
-    $(".output").text(""); //clearing the console
-
-
-    let resp = await fetch('https://codejudge.geeksforgeeks.org/submit-request', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json, text/javascript, */*; q=0.01',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Connection': 'keep-alive',
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Origin': 'https://ide.geeksforgeeks.org',
-            'Referer': 'https://ide.geeksforgeeks.org/',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-site',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
-            'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"'
-        },
-        body: JSON.stringify({
-            'language': $("#languages").val(),
-            'code': editor.getSession().getValue(),
-            'input': document.getElementById("inp").value,  
-            'save': false
-        })
-    });
-   
-    let res = await resp.json();
-    //alert(res.submission_id);
-
-    console.log("fetching..");
-    let opt = await getResult(res);
-
-    let model = await opt.json();
-
-    while(model.status=='in-queue'){
-        opt = await getResult(res);
-        model = await opt.json();
-        console.log("fetching..");
-    }
-
-    if(model.errorCode==""){
-        //document.getElementById("exec").disabled = false;
-        //alert("insideopt");
-        $(".output").text(model.output)
-    }
-    else if(model.rntError!="" && model.rntError!=undefined){
-        //document.getElementById("exec").disabled = false;
-        //alert("insiddere");
-        $(".output").text(model.rntError)
-    }
-    else{
-        //document.getElementById("exec").disabled = false;
-        //alert("inside ce");
-        $(".output").text(model.cmpError)
-    }
-
-    if(model.output!="" && model.output!=undefined){
-        //alert("insideopt");
-        $(".output").text(model.output)
-    }
+        $(".output").text(""); //clearing the console
 
 
-    btnReset(val); // restore original button state
-
-
-
-    // alert("error code");
-    // alert(model.errorCode);
+        let resp = await fetch('https://codejudge.geeksforgeeks.org/submit-request', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/javascript, */*; q=0.01',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Origin': 'https://ide.geeksforgeeks.org',
+                'Referer': 'https://ide.geeksforgeeks.org/',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-site',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+                'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"'
+            },
+            body: JSON.stringify({
+                'language': $("#languages").val(),
+                'code': editor.getSession().getValue(),
+                'input': document.getElementById("inp").value,  
+                'save': false
+            })
+        });
     
-    // alert(model.status);
-    // alert("opt");
-    // alert(model.output);
-    // alert("error");
-    // alert(model.cmpError);
-    // console.log(model);
+        let res = await resp.json();
+        //alert(res.submission_id);
 
-    // let result = await getResult.json();
-    // console.log(result);
-    // alert(result.status);
-    // alert(result.output);
+        console.log("fetching..");
+        let opt = await getResult(res);
+
+        let model = await opt.json();
+
+        while(model.status=='in-queue'){
+            opt = await getResult(res);
+            model = await opt.json();
+            console.log("fetching..");
+        }
+
+        if(model.errorCode==""){
+            //document.getElementById("exec").disabled = false;
+            //alert("insideopt");
+            $(".output").text(model.output)
+        }
+        else if(model.rntError!="" && model.rntError!=undefined){
+            //document.getElementById("exec").disabled = false;
+            //alert("insiddere");
+            $(".output").text(model.rntError)
+        }
+        else{
+            //document.getElementById("exec").disabled = false;
+            //alert("inside ce");
+            $(".output").text(model.cmpError)
+        }
+
+        if(model.output!="" && model.output!=undefined){
+            //alert("insideopt");
+            $(".output").text(model.output)
+        }
+
+
+        btnReset(val); // restore original button state
+
+
+
+        // alert("error code");
+        // alert(model.errorCode);
+        
+        // alert(model.status);
+        // alert("opt");
+        // alert(model.output);
+        // alert("error");
+        // alert(model.cmpError);
+        // console.log(model);
+
+        // let result = await getResult.json();
+        // console.log(result);
+        // alert(result.status);
+        // alert(result.output);
 
 
 
 
-    // $.ajax({
+        // $.ajax({
 
-    //     url: "/ide/app/compiler.php",
+        //     url: "/ide/app/compiler.php",
 
-    //     method: "POST",
+        //     method: "POST",
 
-    //     data: {
-    //         language: $("#languages").val(),
-    //         code: editor.getSession().getValue()
-    //     },
+        //     data: {
+        //         language: $("#languages").val(),
+        //         code: editor.getSession().getValue()
+        //     },
 
-    //     success: function(response) {
-    //         $(".output").text(response)
-    //     }
-    // })
+        //     success: function(response) {
+        //         $(".output").text(response)
+        //     }
+        // })
+    } catch (error) {
+        btnReset(val);
+    }
 }
 
 
